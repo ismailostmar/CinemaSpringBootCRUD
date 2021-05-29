217a25d2bc268fcacce6cb55421e316aefdd6115
DEVGEEKTM <ismailostmar@gmail.com>
2021-05-29 01:15:44 +0100

    MàJ

--- src/main/java/ma/emsi/Cinema/CinemaCRUD/CinemaCrudApplication.java
+++ src/main/java/ma/emsi/Cinema/CinemaCRUD/CinemaCrudApplication.java
@@ -1,37 +1,12 @@
 package ma.emsi.Cinema.CinemaCRUD;
 
-
-import ma.emsi.Cinema.CinemaCRUD.Entites.Film;
-import ma.emsi.Cinema.CinemaCRUD.Services.ICinemaInitService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 
 @SpringBootApplication
-public class CinemaCrudApplication implements CommandLineRunner {
-
-	@Autowired private ICinemaInitService CinemaInitService;
-	@Autowired	private RepositoryRestConfiguration restConfiguration;
+public class CinemaCrudApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CinemaCrudApplication.class, args);
-	}
+		SpringApplication.run(CinemaCrudApplication.class, args); }
 
-	@Override
-	public void run(String... args) throws Exception {
-
-		restConfiguration.exposeIdsFor(Film.class);
-		CinemaInitService.initVilles();
-		CinemaInitService.initCinema();
-		CinemaInitService.initFilms();
-		CinemaInitService.initSeances();
-		CinemaInitService.initCategories();
-		CinemaInitService.initPlaces();
-		CinemaInitService.initProjections();
-		CinemaInitService.initTickets();
-		CinemaInitService.initSalles();
-		System.out.println("Initialisation has been Complete ! ");
-	}
 }
