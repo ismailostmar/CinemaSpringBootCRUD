647f0a9c04bb006e394ba0100894684c7660c494
DEVGEEKTM <ismailostmar@gmail.com>
2021-07-01 23:41:21 +0100

    Stuffs

--- src/main/java/ma/emsi/Cinema/CinemaCRUD/Controller/CinemaRestController.java
+++ src/main/java/ma/emsi/Cinema/CinemaCRUD/Controller/CinemaRestController.java
@@ -18,7 +18,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-@CrossOrigin("*")
+@CrossOrigin("http://localhost:4200")
 public class CinemaRestController {
 
     @Autowired
