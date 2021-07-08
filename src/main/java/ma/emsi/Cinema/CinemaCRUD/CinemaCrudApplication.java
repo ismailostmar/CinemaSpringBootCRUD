495995baafba6d32785aeab85252eb8ee4fc9c1c
DEVGEEKTM <ismailostmar@gmail.com>
2021-07-08 12:33:57 +0100

    Rest Config

--- src/main/java/ma/emsi/Cinema/CinemaCRUD/CinemaCrudApplication.java
+++ src/main/java/ma/emsi/Cinema/CinemaCRUD/CinemaCrudApplication.java
@@ -1,22 +1,27 @@
 package ma.emsi.Cinema.CinemaCRUD;
 
+import ma.emsi.Cinema.CinemaCRUD.Entites.Film;
 import ma.emsi.Cinema.CinemaCRUD.Services.ICinemaInitService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 
 @SpringBootApplication
 public class CinemaCrudApplication implements CommandLineRunner {
 	@Autowired
 	private ICinemaInitService cinemaInitService;
 
+	@Autowired
+	private RepositoryRestConfiguration restConfiguration;
 	public static void main(String[] args) {
 		SpringApplication.run(CinemaCrudApplication.class, args); }
 
 	@Override
 	public void run(String... args) throws Exception
 	{
+		restConfiguration.exposeIdsFor(Film.class);
 		cinemaInitService.initVilles();
 		cinemaInitService.initCinema();
 		cinemaInitService.initSalles();
